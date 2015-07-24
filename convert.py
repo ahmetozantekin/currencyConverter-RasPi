015f0c8ce7484963c99e242bf28187a6c6c4dc4c
Ahmet Ozan Tekin <ahmetozantekin@gmail.com>
2015-07-24 20:09:04 +0300

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 
 """
-Yahoo Finance API kullanarak paranın degerini anlik olarak gösterebilen
-RaspberryPi üzerinde çalışan bir uygulama geliştirmesi.
+finance.yahoo.com, http://finance.yahoo.com/d/quotes.csv?e=.csv&f=sl1d1t1&s={?}{?}=X
 
-finance.yahoo.com'un, http://finance.yahoo.com/d/quotes.csv?e=.csv&f=sl1d1t1&s={?}{?}=X
-url'sini kullanacagiz.
 """
 
 import urllib2
@@ -14,49 +11,47 @@ import xml.dom from minidom
 
 URL = 'http://finance.yahoo.com/d/quotes.csv?e=.csv&f=sl1d1t1&s={FROM}{TO}=X'
 
-def para_degeri(url):
+def priceTag(url):
     # request -> response
     request = urllib2.Request(url)
     
     try:
         response = urllib2.urlopen(request)
         
-    except urllib2.URLError: ## URL hatalı ise hiç bir şey yapmasın.
+    except urllib2.URLError: ## do nothing
         return None
     
     sonuc = response.read()
     return sonuc
 
 
-def cevir(FROM, TO, kacPARA):
+def con(FROM, TO, MUCH):
     if FROM.lower() == TO.lower():
         return "Farklı para birimleri giriniz."
     
-    data = para_degeri(URL.format(FROM, TO))
-    #URL'de FROM ve TO yazan yerlere fonksiyonun parametrelerini yerlestirir
+    data = priceTag(URL.format(FROM, TO))
+    #URL ~> FROM ve TO 
     
     if data:
-        parcala = data.split(',')
+        S = data.split(',')
         """
-        URL cikti olarak 'USDTRY=X, 2.7410, "7/24/2015", "8:08am"'
-        gibi bir cikti veriyor. Bunu virgüllerine gore parcalayıp
-        dizinin 1. elemanini alirsak, paranin degerini elde ederiz.
+        USDTRY=X, 2.7410, "7/24/2015", "8:08am"'
         """
         
         try:
-            return kacPARA + FROM + parcala[1]*kacPARA + TO
-            #Örnek:   5      USD       2,74*5=13,5       TRY
+            return MUCH + FROM + s[1]*kacPARA + TO
+            #EXMP.:   5      USD       2,74*5=13,5       TRY
             
         except (IndexError, ValueError):
-            return "Yanlis para birimi girisi."
+            return "Wrong Currency."
         
 
 ##TEST
 
 print "Para Birimleri: USD, GBP, JPY, TRY, EUR, AUD, INR ..."
     
-_from = input("Degeri cevrilecek para:(FROM) ")
-_to   = input("Degere uyarlanacak para: (TO) ")
-_para = input("kac para cevrilecek: ")
+_from = input("FROM ")
+_to   = input("TO ")
+_para = input("MUCH: ")
     
-cevir(_from, _to, _para)
+con(_from, _to, _para)
